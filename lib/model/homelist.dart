e0cb1ecf96642177756f3a2cbf5bfe92bdd45d26
Talha Muhammad <121675123+talha1230@users.noreply.github.com>
2024-12-26 20:09:28 +0800

    uploaded and Add configuration files and initial setup for Flutter projects across platforms

---
+++ lib/model/homelist.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/widgets.dart';
+import 'package:best_flutter_ui_templates/fitness_app/fitness_app_home_screen.dart';
+
+class HomeList {
+  HomeList({
+    this.navigateScreen,
+    this.imagePath = '',
+  });
+
+  Widget? navigateScreen;
+  String imagePath;
+
+  static List<HomeList> homeList = [
+    HomeList(
+      imagePath: 'assets/fitness_app/fitness_app.png',
+      navigateScreen: FitnessAppHomeScreen(),
+    ),
+    // Commenting out unavailable screens
+    // HomeList(
+    //   imagePath: 'assets/hotel/hotel_booking.png',
+    //   navigateScreen: HotelHomeScreen(),
+    // ),
+    // HomeList(
+    //   imagePath: 'assets/design_course/design_course.png',
+    //   navigateScreen: DesignCourseHomeScreen(),
+    // ),
+  ];
+}
